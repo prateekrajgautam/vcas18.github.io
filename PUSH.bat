96253a39cce82e6e8b2dd392b42a35d0c9f87f30
prateekrajgautam <prateekrajgautam@gmail.com>
2018-07-07 13:17:05 +0530

    update push.bat

--- PUSH.bat
+++ PUSH.bat
@@ -1,3 +1,11 @@
+@echo git add .
 git add .
-git commit -m"template download form own server"
+@echo off
+title git push enter commit message
+@echo Enter commit message
+set /p commit=
+@echo git commit
+git commit -m"%commit%"
+@echo git push
 git push
+@echo completed
